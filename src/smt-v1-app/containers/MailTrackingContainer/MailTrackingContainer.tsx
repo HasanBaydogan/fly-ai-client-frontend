ae0c187d599d7df8a3c610b0e85101400246756f
Özgürhan Polat <ozgurhan.45@gmail.com>
2025-01-02 15:01:45 +0300

    pagination is added

--- src/smt-v1-app/containers/MailTrackingContainer/MailTrackingContainer.tsx
+++ src/smt-v1-app/containers/MailTrackingContainer/MailTrackingContainer.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Nav, Tab, Table } from 'react-bootstrap';
+import { Nav, Pagination, Tab, Table } from 'react-bootstrap';
 import { useDispatch } from 'react-redux';
 import LoadingAnimation from 'smt-v1-app/components/common/LoadingAnimation/LoadingAnimation';
 import MailTrackingHeader from 'smt-v1-app/components/features/MailTrackingHeader/MailTrackingHeader';
@@ -15,6 +15,11 @@ import {
 } from 'smt-v1-app/services/MailTrackingService';
 import { getActiveStatusFromStringStatus } from './MailTrackingHelper';
 import './MailTrackingContainer.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faChevronLeft,
+  faChevronRight
+} from '@fortawesome/free-solid-svg-icons';
 
 interface RFQMailRow {
   rfqMailId: string;
@@ -78,8 +83,10 @@ const MailTrackingContainer = () => {
 
   // page no (state)
   const [pageNo, setPageNo] = useState(1);
+  const [totalPage, setTotalPage] = useState(0);
   // page size (state)
   const [pageSize, setPageSize] = useState(10);
+  const [totalRFQMail, setTotalRFQMail] = useState(0);
 
   // page activestatus(state)
   const [filterType, setFilterType] = useState<filterType>('ALL');
@@ -95,8 +102,8 @@ const MailTrackingContainer = () => {
 
   // useEffect data fetch
   useEffect(() => {
-    setLoading(true);
     const getRFQMailsFromDB = async () => {
+      setLoading(true);
       const formattedDate = sinceFromDate.toLocaleDateString('tr-TR', {
         day: '2-digit',
         month: '2-digit',
@@ -110,15 +117,33 @@ const MailTrackingContainer = () => {
       );
       console.log(response);
       dispatch(deleteRfqMails());
-      dispatch(putRfqMails(response.data));
+      dispatch(putRfqMails(response.data.rfqMailPreviewResponses));
+      setTotalRFQMail(response.data.totalItems);
+      setTotalPage(response.data.totalPage);
+      setLoading(false);
     };
     getRFQMailsFromDB();
-    setLoading(false);
   }, [pageNo, pageSize, filterType, sinceFromDate]);
 
   // handle Not RFQ , No Quote and Spam icons
   // handle refresh button
   // onSelect data fetching
+
+  const handlePageChange = (pageNumber: number) => {
+    setPageNo(pageNumber);
+  };
+
+  const handlePrevPage = () => {
+    if (pageNo > 1) {
+      setPageNo(pageNo - 1);
+    }
+  };
+
+  const handleNextPage = () => {
+    if (pageNo < totalPage) {
+      setPageNo(pageNo + 1);
+    }
+  };
 
   const handleRFQNumberIdSearch = async (rfqNumberId: string) => {
     setLoading(true);
@@ -133,6 +158,7 @@ const MailTrackingContainer = () => {
     if (stringStatus) {
       setStringActiveStatus(stringStatus); // Update the active tab key
       const filterType = getActiveStatusFromStringStatus(stringStatus);
+      setPageNo(1);
       setFilterType(filterType);
     }
   };
@@ -180,12 +206,70 @@ const MailTrackingContainer = () => {
                 </thead>
                 <tbody>
                   {reduxRfqMails &&
-                    reduxRfqMails.map(rfqMail => (
-                      <RfqMailRowItem rfqMail={rfqMail} />
+                    reduxRfqMails.map((rfqMail, key) => (
+                      <RfqMailRowItem key={key} rfqMail={rfqMail} />
                     ))}
                 </tbody>
               </Table>
             )}
+            <Pagination className="mb-2 justify-content-end">
+              <Pagination.Prev onClick={handlePrevPage} disabled={pageNo === 1}>
+                <FontAwesomeIcon icon={faChevronLeft} />
+              </Pagination.Prev>
+
+              {/* First 3 Pages */}
+              {Array.from({ length: Math.min(3, totalPage) }, (_, index) => (
+                <Pagination.Item
+                  key={index + 1}
+                  active={index + 1 === pageNo}
+                  onClick={() => handlePageChange(index + 1)}
+                >
+                  {index + 1}
+                </Pagination.Item>
+              ))}
+
+              {/* Ellipsis if necessary */}
+              {pageNo > 4 && totalPage > 6 && <Pagination.Ellipsis disabled />}
+
+              {/* Current Page (if not part of the first/last 3) */}
+              {pageNo > 3 && pageNo < totalPage - 2 && (
+                <Pagination.Item
+                  active
+                  onClick={() => handlePageChange(pageNo)}
+                >
+                  {pageNo}
+                </Pagination.Item>
+              )}
+
+              {/* Ellipsis before last pages */}
+              {pageNo < totalPage - 3 && totalPage > 6 && (
+                <Pagination.Ellipsis disabled />
+              )}
+
+              {/* Last 3 Pages */}
+              {Array.from({ length: 3 }, (_, index) => {
+                const pageIndex = totalPage - 3 + index;
+                if (pageIndex + 1 > 3) {
+                  return (
+                    <Pagination.Item
+                      key={pageIndex + 1}
+                      active={pageIndex + 1 === pageNo}
+                      onClick={() => handlePageChange(pageIndex + 1)}
+                    >
+                      {pageIndex + 1}
+                    </Pagination.Item>
+                  );
+                }
+                return null;
+              })}
+
+              <Pagination.Next
+                onClick={handleNextPage}
+                disabled={pageNo === totalPage}
+              >
+                <FontAwesomeIcon icon={faChevronRight} />
+              </Pagination.Next>
+            </Pagination>
           </Tab.Pane>
         </Tab.Content>
       </Tab.Container>
