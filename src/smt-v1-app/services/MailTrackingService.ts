37e7c45b7933aeea0441b48331b08e89f209f8e2
HasanBaydogan <hasanbaydogan20@gmail.com>
2025-03-24 10:16:48 +0300

    merge

--- src/smt-v1-app/services/MailTrackingService.ts
+++ src/smt-v1-app/services/MailTrackingService.ts
@@ -54,84 +54,7 @@ export const cancelRFQMail = async (rfqMailId: string) => {
 
 // RFQ Mail OPEN -> WFS transition
 export const convertOpenToWFS = async (rfqMailId: string) => {
-<<<<<<< HEAD
   return await putRequest(`/rfq-mail/status-to-wfs/${rfqMailId}`, {});
-=======
-  try {
-    const accessToken = Cookies.get('access_token');
-    const headers = {};
-    if (accessToken) {
-      headers['Authorization'] = `Bearer ${accessToken}`;
-    } else {
-      window.location.assign('/');
-    }
-
-    const response = await api().put(
-      `/rfq-mail/status-to-wfs/${rfqMailId}`,
-      {},
-      {
-        headers
-      }
-    );
-    // console.log('Response', response);
-
-    if (response.status === 200) {
-      if (response.data.statusCode === 498) {
-        // Expired JWT
-        try {
-          const refreshTokenresponse = await api().post('/auth/refresh-token', {
-            refresh_token: Cookies.get('refresh_token')
-          });
-          if (refreshTokenresponse.data.statusCode === 200) {
-            setCookie(
-              'access_token',
-              refreshTokenresponse.data.data.access_token
-            );
-            setCookie(
-              'refresh_token',
-              refreshTokenresponse.data.data.refresh_token
-            );
-            let rfqMailResponseAfterRefresh = await api().put(
-              `/rfq-mail/status-to-wfs/${rfqMailId}`,
-              {},
-              {
-                headers: {
-                  Authorization: `Bearer ${refreshTokenresponse.data.accessToken}`
-                }
-              }
-            );
-
-            return rfqMailResponseAfterRefresh.data;
-          } else if (refreshTokenresponse.data.statusCode === 498) {
-            // Expired Refresh Token
-            Cookies.remove('access_token');
-            Cookies.remove('refresh_token');
-            window.location.assign('/');
-          } else if (refreshTokenresponse.data.statusCode === 411) {
-            // Invalid Refresh Token
-            Cookies.remove('access_token');
-            Cookies.remove('refresh_token');
-            window.location.assign('/');
-          } else if (refreshTokenresponse.data.statusCode === 404) {
-            console.log('User not found');
-            Cookies.remove('access_token');
-            Cookies.remove('refresh_token');
-            window.location.assign('/');
-          }
-        } catch (err) {
-          console.log(err);
-        }
-      } else if (response.data.statusCode === 401) {
-        Cookies.remove('access_token');
-        Cookies.remove('refresh_token');
-        window.location.assign('/');
-      }
-      return response.data;
-    }
-  } catch (err) {
-    console.log('RFQMail Detail Permission Error');
-  }
->>>>>>> fix/20march
 };
 
 // RFQ Maili revert
@@ -150,154 +73,18 @@ export const searchRFQMailBySubject = async (
   );
 };
 
+//RFQ Mail Filter
 export const getByMailFilterItems = async (dateFrom: string) => {
-  try {
-    const accessToken = Cookies.get('access_token');
-    const headers = {};
-    if (accessToken) {
-      headers['Authorization'] = `Bearer ${accessToken}`;
-    } else {
-      window.location.assign('/');
-    }
-
-    const response = await api().get(
-      `/rfq-mail/filter-parameters?dateFrom=${dateFrom}`,
-      {
-        headers
-      }
-    );
-    if (response.data.statusCode === 200) {
-      return response.data;
-    } else if (response.data.statusCode === 498) {
-      // Expired JWT
-      try {
-        const refreshTokenresponse = await api().post('/auth/refresh-token', {
-          refresh_token: Cookies.get('refresh_token')
-        });
-        if (refreshTokenresponse.data.statusCode === 200) {
-          setCookie(
-            'access_token',
-            refreshTokenresponse.data.data.access_token
-          );
-          setCookie(
-            'refresh_token',
-            refreshTokenresponse.data.data.refresh_token
-          );
-          let rfqMailResponseAfterRefresh = await api().get(
-            `/rfq-mail/filter-parameters?dateFrom=${dateFrom}`,
-            {
-              headers: {
-                Authorization: `Bearer ${refreshTokenresponse.data.accessToken}`
-              }
-            }
-          );
-
-          return rfqMailResponseAfterRefresh.data;
-        } else if (refreshTokenresponse.data.statusCode === 498) {
-          // Expired Refresh Token
-          Cookies.remove('access_token');
-          Cookies.remove('refresh_token');
-          window.location.assign('/');
-        } else if (refreshTokenresponse.data.statusCode === 411) {
-          // Invalid Refresh Token
-          Cookies.remove('access_token');
-          Cookies.remove('refresh_token');
-          window.location.assign('/');
-        } else if (refreshTokenresponse.data.statusCode === 404) {
-          console.log('User not found');
-          Cookies.remove('access_token');
-          Cookies.remove('refresh_token');
-          window.location.assign('/');
-        }
-      } catch (err) {
-        console.log(err);
-      }
-    } else if (response.data.statusCode === 401) {
-      Cookies.remove('access_token');
-      Cookies.remove('refresh_token');
-      window.location.assign('/');
-    }
-  } catch (err) {
-    console.log('getByMailFilterItems Permission Error');
-  }
+  return await getRequest(`/rfq-mail/filter-parameters?dateFrom=${dateFrom}`);
 };
 
+//RFQ Mail Filter Parameters
 export const searchByFilterParameters = async (
   pageNo: number,
   pageSize: number,
   params: string
 ) => {
-  console.log('pageNo', pageNo);
-  console.log('params', params);
-
-  try {
-    const accessToken = Cookies.get('access_token');
-    const headers = {};
-    if (accessToken) {
-      headers['Authorization'] = `Bearer ${accessToken}`;
-    } else {
-      window.location.assign('/');
-    }
-
-    const rfqMailResp = await api().get(
-      `/rfq-mail/search-filter-parameters/${pageNo}/${pageSize}?${params}`,
-      {
-        headers
-      }
-    );
-    console.log('rfqMailResp', rfqMailResp);
-    if (rfqMailResp.data.statusCode === 200) {
-      return rfqMailResp.data;
-    } else if (rfqMailResp.data.statusCode === 498) {
-      // Expired JWT
-      try {
-        const refreshTokenresponse = await api().post('/auth/refresh-token', {
-          refresh_token: Cookies.get('refresh_token')
-        });
-        if (refreshTokenresponse.data.statusCode === 200) {
-          setCookie(
-            'access_token',
-            refreshTokenresponse.data.data.access_token
-          );
-          setCookie(
-            'refresh_token',
-            refreshTokenresponse.data.data.refresh_token
-          );
-          let rfqMailResponseAfterRefresh = await api().get(
-            `/rfq-mail/search-filter-parameters/${pageNo}/${pageSize}/${params}`,
-            {
-              headers: {
-                Authorization: `Bearer ${refreshTokenresponse.data.accessToken}`
-              }
-            }
-          );
-
-          return rfqMailResponseAfterRefresh.data;
-        } else if (refreshTokenresponse.data.statusCode === 411) {
-          // Invalid Refresh Token
-          Cookies.remove('access_token');
-          Cookies.remove('refresh_token');
-          window.location.assign('/');
-        } else if (refreshTokenresponse.data.statusCode === 498) {
-          // Expired Refresh Token
-          Cookies.remove('access_token');
-          Cookies.remove('refresh_token');
-          window.location.assign('/');
-        } else if (refreshTokenresponse.data.statusCode === 404) {
-          console.log('User not found');
-          Cookies.remove('access_token');
-          Cookies.remove('refresh_token');
-          window.location.assign('/');
-        }
-      } catch (err) {
-        console.log(err);
-      }
-    } else if (rfqMailResp.data.statusCode === 401) {
-      Cookies.remove('access_token');
-      Cookies.remove('refresh_token');
-      window.location.assign('/');
-    }
-  } catch (err) {
-    console.log('searchByFilterParameters Permission Error');
-  }
+  return await getRequest(
+    `/rfq-mail/search-filter-parameters/${pageNo}/${pageSize}?${params}`
+  );
 };
