1bd3b0d50eeaf36147e5798871908715d59f9f51
Özgürhan Polat <ozgurhan.45@gmail.com>
2025-06-15 22:05:07 +0300

    Live server build is made in 15.06.2025

--- src/smt-v1-app/services/ApiConstants.ts
+++ src/smt-v1-app/services/ApiConstants.ts
@@ -1,3 +1,3 @@
 //export const baseURL = 'http://localhost:8080/api/v1';   //local db
-// export const baseURL = 'https://app.asparel.com.tr:8080/api/v1'; //real db
-export const baseURL = 'https://test.flyai.tr:8080/api/v1'; //Mock db
+export const baseURL = 'https://app.asparel.com.tr:8080/api/v1'; //real db
+//export const baseURL = 'https://test.flyai.tr:8080/api/v1'; //Mock db
