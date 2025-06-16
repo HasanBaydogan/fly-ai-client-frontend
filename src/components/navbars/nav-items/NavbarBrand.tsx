43a1fcc6b5be6a8c62bb7011179750583d8c039b
HasanBaydogan <hasanbaydogan20@gmail.com>
2025-06-16 17:23:32 +0300

    Pi list action front düzenlemesi ve heartbeat kaldırıldı.

--- src/components/navbars/nav-items/NavbarBrand.tsx
+++ src/components/navbars/nav-items/NavbarBrand.tsx
@@ -46,7 +46,7 @@ const NavbarBrand = ({ logo }: { logo?: string }) => {
             </div>
           )}
           <div className="small fs-10 text-center  text-muted">
-            v1.13.7-13.06.25
+            v1.13.10-16.06.25
           </div>
         </Navbar.Brand>
       </div>
