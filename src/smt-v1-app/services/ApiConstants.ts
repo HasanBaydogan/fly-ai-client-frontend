3fdf486e5566437cf10c1a698805eba2f030e8bc
HasanBaydogan <hasanbaydogan20@gmail.com>
2025-06-18 11:02:41 +0300

    Live update ready 2

--- src/smt-v1-app/services/ApiConstants.ts
+++ src/smt-v1-app/services/ApiConstants.ts
@@ -1,7 +1,3 @@
 //export const baseURL = 'http://localhost:8080/api/v1';   //local db
 export const baseURL = 'https://app.asparel.com.tr:8080/api/v1'; //real db
-<<<<<<< HEAD
 //export const baseURL = 'https://test.flyai.tr:8080/api/v1'; //Mock db
-=======
-// export const baseURL = 'https://test.flyai.tr:8080/api/v1'; //Mock db
->>>>>>> ccfa848b4727670af8cafdd7f3e610268491f441
